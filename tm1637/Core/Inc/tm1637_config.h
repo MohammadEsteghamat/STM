d7a1a0f73d58fc85abbc861d89ec424e749f8890
MohammadEsteghamt <tradesteghamat@gmail.com>
2025-08-07 18:36:26 +0330

    Initializing 7-Segment Display with TM1637

---
+++ tm1637/Core/Inc/tm1637_config.h
@@ -0,0 +1,8 @@
+#ifndef _TM1637_CONFIG_H_
+#define _TM1637_CONFIG_H_
+
+#define _TM1637_FREERTOS     0
+#define SYSTEM_CLOCK_MHZ     4
+#define _TM1637_BIT_DELAY    (20 * SYSTEM_CLOCK_MHZ / 8)
+
+#endif
